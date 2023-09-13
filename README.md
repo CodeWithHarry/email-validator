6701fcaa85e1f12d4f202b9dfb4bb50ebcc92556
CodeWithHarry <48705673+CodeWithHarry@users.noreply.github.com>
2023-09-13 18:59:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# email-validator-html-css-js
+ This project can be used to validate emails using the emailvalidation api
---
+++ js/index.js
@@ -0,0 +1,45 @@
+console.log("This is my script")
+
+let result = {
+    "tag": "",
+    "free": false,
+    "role": false,
+    "user": "akshaykumar",
+    "email": "akshaykumar@codewithharry.com",
+    "score": 0.64,
+    "state": "undeliverable",
+    "domain": "codewithharry.com",
+    "reason": "invalid_mailbox",
+    "mx_found": true,
+    "catch_all": null,
+    "disposable": false,
+    "smtp_check": false,
+    "did_you_mean": "",
+    "format_valid": true
+}
+
+
+
+submitBtn.addEventListener("click", async (e) => {
+    e.preventDefault()
+    console.log("Clicked!")
+    resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="">`
+    let key = "ema_live_ITkV5Pqq40WNdMLdjqMZlNcb4AEBrNvWiSFd1MYE"
+    let email = document.getElementById("username").value 
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+    let res = await fetch(url)
+    let result = await res.json()
+    let str = ``
+    for (key of Object.keys(result)) {
+        if(result[key] !== "" && result[key]!== " "){ 
+            str = str + `<div>${key}: ${result[key]}</div>`
+        }
+    }
+
+    console.log(str)
+    resultCont.innerHTML = str
+})
+
+
+
+
