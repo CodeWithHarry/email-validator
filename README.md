0808924427ceb824fe10ed23fcc6e9df5fa4f806
Arin Chaudhary <147314012+CodeWithMe04@users.noreply.github.com>
2024-01-18 10:59:11 +0530

    Adding files, Removing files and using TailwindCSS

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# email-validator-html-css-js
- This project can be used to validate emails using the emailvalidation api
+# Email-Validator-html-TailwindCSS-js
+This project can be used to validate emails using the emailvalidation API.
--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-console.log("This is my script")
-
 let result = {
     "tag": "",
     "free": false,
@@ -18,27 +16,50 @@ let result = {
     "format_valid": true
 }
 
-
+// Add this at the beginning of your js/index.js file
+const submitBtn = document.getElementById("submitBtn");
+const resultCont = document.getElementById("resultCont");
 
 submitBtn.addEventListener("click", async (e) => {
-    e.preventDefault()
-    console.log("Clicked!")
-    resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="">`
-    let key = "ema_live_ITkV5Pqq40WNdMLdjqMZlNcb4AEBrNvWiSFd1MYE"
-    let email = document.getElementById("username").value 
-    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
-    let res = await fetch(url)
-    let result = await res.json()
-    let str = ``
+    e.preventDefault();
+    console.log("Clicked!");
+    resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="">`;
+    let key = "YOUR_API_KEY";
+    let email = document.getElementById("username").value;
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`;
+    let res = await fetch(url);
+    let result = await res.json();
+    let str = ``;
     for (key of Object.keys(result)) {
-        if(result[key] !== "" && result[key]!== " "){ 
-            str = str + `<div>${key}: ${result[key]}</div>`
+        if (result[key] !== "" && result[key] !== " ") {
+            str = str + `<div>${key}: ${result[key]}</div>`;
         }
     }
 
-    console.log(str)
-    resultCont.innerHTML = str
-})
+    console.log(str);
+    resultCont.innerHTML = str;
+});
+
+
+// submitBtn.addEventListener("click", async (e) => {
+//     e.preventDefault()
+//     console.log("Clicked!")
+//     resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="">`
+//     let key = "ema_live_ITkV5Pqq40WNdMLdjqMZlNcb4AEBrNvWiSFd1MYE"
+//     let email = document.getElementById("username").value 
+//     let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+//     let res = await fetch(url)
+//     let result = await res.json()
+//     let str = ``
+//     for (key of Object.keys(result)) {
+//         if(result[key] !== "" && result[key]!== " "){ 
+//             str = str + `<div>${key}: ${result[key]}</div>`
+//         }
+//     }
+
+//     console.log(str)
+//     resultCont.innerHTML = str
+// })
 
 
 
